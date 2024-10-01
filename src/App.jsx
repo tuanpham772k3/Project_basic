572c0d286e8dc205b4f6f480bafb7e6f43170357
tuanpham772k3 <tuanphamu23@gmail.com>
2024-10-01 22:07:46 +0700

    navbar-header-complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./home/Home";
 import Hotel from "./hotel/Hotel";
 import List from "./list/List";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 function App() {
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "./Header.scss";
 
 export default function Header() {
     return (
-        <header class="header">
-            <div class="header__logo">Logo</div>
-            <nav class="header__nav">
-                <ul class="header__nav-list">
-                    <li class="header__nav-item">Home</li>
-                    <li class="header__nav-item">About</li>
+        <header className="header">
+            <nav className="nav">
+                <ul className="nav-list">
+                    <li className="nav-item"><a href="#"><i className="fas fa-bed"></i> Stays</a></li>
+                    <li className="nav-item"><a href="#"><i className="fas fa-plane"></i> Flights</a></li>
+                    <li className="nav-item"><a href="#"><i className="fas fa-car"></i> Car rentals</a></li>
+                    <li className="nav-item"><a href="#"><i className="fas fa-landmark"></i> Attractions</a></li>
+                    <li className="nav-item"><a href="#"><i className="fas fa-taxi"></i> Airport taxis</a></li>
                 </ul>
             </nav>
-            <h2 className='header__title'>Chỉ có 1 lần sống, hãy sống hết mình</h2>
-            <p className="header__intro-text">cha yo</p>
-            <button className="header__button">Sign in/ Register</button>
-            <button className="navButton">Register</button>
+            <div className="header-content">
+                <h1 className="header-title">A lifetime of discounts? It's Genius.</h1>
+                <p className="header-subtitle">Get rewarded for your travels – unlock instant savings of 10% or more with a free Lamabooking account.</p>
+                <a href="#" className="btn-signin">Sign in / Register</a>
+            </div>
         </header>
-
-    )
+    );
 }
